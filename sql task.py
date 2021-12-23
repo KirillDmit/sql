e728457c6224e6639e45dee11ede50b54bd43fcf
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-23 13:38:14 +0500

    Update sql task.py

--- sql task.py
+++ sql task.py
@@ -7,3 +7,27 @@ res = con.execute('SELECT COUNT(*) FROM works')
 print(list(res))
 
 
+res = cur.execute('SELECT COUNT(*) FROM works')
+for e in res:
+    print(e)
+
+res = cur.execute('SELECT COUNT(*) from works where gender = "Женский"')
+for e in res:
+    print(e)
+
+res = cur.execute('SELECT COUNT(*) from works where gender = "Мужской"')
+for e in res:
+    print(e)
+
+res = cur.execute('SELECT COUNT(*) from works where skills not null ')
+for e in res:
+    print(e)
+
+res = cur.execute('SELECT skills from works where skills not null ')
+for e in res:
+    print(e)
+
+res = cur.execute('SELECT salary from works where skills like "%Python%"')
+for e in res:
+    print(e)
+
