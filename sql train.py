ad3ef3fb461ea5b8ce48fb15410476be6dbccebd
KirillDmit <kirilldmitriev20015@gmail.com>
2021-12-23 13:38:17 +0500

    Update sql train.py

--- sql train.py
+++ sql train.py
@@ -2,6 +2,19 @@ import sqlite3
 
 con = sqlite3.connect('works.sqlite')
 cur = con.cursor()
+
+cur.execute('CREATE TABLE works('
+            'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+            ' salary INTEGER,'
+            'educationType TEXT,'
+            'jobTitle TEXT,'
+            'qualification TEXT,'
+            'gender TEXT,'
+            'dateModify TEXT,'
+            'skills TEXT,'
+            'otherInfo TEXT'
+            )
+
 res = con.execute('SELECT COUNT(*) FROM works')
 list(res)
 res = con.execute('select * from works limit 3')
@@ -10,6 +23,5 @@ for r in res:
     print(r)
 
 for row in cur.execute('SELECT * FROM stocks ORDER BY salary'):
-        print(row)
-
+    print(row)
 
